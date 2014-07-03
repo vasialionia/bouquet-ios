4d67c50fd503fc72f69980a5773accf2ba1951a1
drif <drif@drifs-MacBook-Pro.local>
2014-07-03 15:07:34 +0300

    updated logging

--- Bouquet/Common/BQMacro.h
+++ Bouquet/Common/BQMacro.h
@@ -5,7 +5,41 @@
 // Copyright (c) 2014 vasialionia. All rights reserved.
 //
 
-#define BQAssert NSAssert
-#define BQParameterAssert NSParameterAssert
+#if !DEBUG
+#import "MWLogging.h"
+#import <Crashlytics/Crashlytics.h>
+#endif
 
+#if DEBUG
 #define BQLogError NSLog
+#else
+#define BQLogError(...) \
+do \
+{ \
+    NSString *message = [NSString stringWithFormat:__VA_ARGS__]; \
+    MWLogError(@"%@ in %s of %s:%u", message, __PRETTY_FUNCTION__, __FILE__, __LINE__); \
+    CLS_LOG(@"<Error> %@", message); \
+} \
+while(0)
+#endif
+
+#if DEBUG
+#define BQAssert NSAssert
+#else
+#define BQAssert(condition, ...) \
+do \
+{ \
+    if(!(condition)) \
+    { \
+        NSString *assertmessage = [NSString stringWithFormat:__VA_ARGS__]; \
+        BQLogError(@"<Assert> %@", assertmessage); \
+    } \
+} \
+while(0)
+#endif
+
+#if DEBUG
+#define BQParameterAssert NSParameterAssert
+#else
+#define BQParameterAssert(condition) NSAssert((condition), @"Invalid parameter not satisfying: %s", #condition)
+#endif
